e5604200ee9c8402ff43f3d43b406ca869388113
Fernando Rocha <34967844+Fernando-A-Rocha@users.noreply.github.com>
2024-06-24 21:07:29 +0100

    Update c_replace.lua

--- nando_crypt-example/c_replace.lua
+++ nando_crypt-example/c_replace.lua
@@ -2,25 +2,43 @@
 Example resource that uses NandoCrypt to decrypt models and replace them.
 ]]
 
-local replaceList = { -- Possible keys: model, txd, dff, col, lodDistance
+local NANDOCRYPT_EXT = ".nandocrypt"
+
+-- Possible keys: model, txd, dff, col, lodDistance
+local replaceList = {
     {model=507, txd="mods/elegant.txd.nandocrypt", dff="mods/elegant.dff.nandocrypt"},
     {model=955, txd="mods/sprunk.txd.nandocrypt", dff="mods/sprunk.dff.nandocrypt"},
 }
 
-function checkReplaceCompleted(theType)
-	local count = 0
-	for k,v in pairs(replaceList) do
-		if (not v[theType.."_loaded"]) then
-			return false
-		else
-			count = count + 1
-		end
-	end
-	print("Replaced a total of "..count.." "..string.upper(theType).." mod files")
-	return true
+local function isEncrypted(filePath)
+    return filePath:sub(-#NANDOCRYPT_EXT) == NANDOCRYPT_EXT
 end
 
-function replaceMods(theType)
+local function applyMod(data, theType, model)
+    if theType == "txd" then
+        return engineImportTXD(engineLoadTXD(data), model)
+    elseif theType == "dff" then
+        return engineReplaceModel(engineLoadDFF(data), model)
+    elseif theType == "col" then
+        return engineReplaceCOL(engineLoadCOL(data), model)
+    end
+end
+
+local function loadOneMod(theType, path, model)
+    if isEncrypted(path) then
+        if not ncDecrypt(path,
+            function(data)
+                applyMod(data, theType, model)
+            end
+        ) then
+            outputDebugString("Decryption of '"..path.."' failed", 1)
+        end
+    else
+        applyMod(path, theType, model)
+    end
+end
+
+local function replaceMods()
 
 	for k,v in pairs(replaceList) do
         local model = v.model
@@ -29,53 +47,22 @@ function replaceMods(theType)
         	return
         end
 
-        local path = v[theType]
-        if type(path)=="string" then
-        	local worked
-			if theType == "txd" then
-                worked = ncDecrypt(path,
-                    function(data)
-                        if engineImportTXD(engineLoadTXD(data), model) then
-                        	print(("Replaced %s (%s) for model %d"):format(path, string.upper(theType), model))
-                        end
-                        replaceList[k][theType.."_loaded"] = true
-                        if checkReplaceCompleted("txd") then replaceMods("dff") end
-                    end
-                )
-			elseif theType == "dff" then
-                worked = ncDecrypt(path,
-                    function(data)
-                        if engineReplaceModel(engineLoadDFF(data), model) then
-                        	print(("Replaced %s (%s) for model %d"):format(path, string.upper(theType), model))
-                        end
-                        replaceList[k][theType.."_loaded"] = true
-                        if checkReplaceCompleted("dff") then replaceMods("col") end
-                    end
-                )
-                if not worked then
-                	outputDebugString("ABORTING: Decryption of '"..path.."' failed", 1)
-                    return
-                end
-			elseif theType == "col" then
-				worked = ncDecrypt(path,
-                    function(data)
-                        if engineReplaceCOL(engineLoadCOL(data), model) then
-                        	print(("Replaced %s (%s) for model %d"):format(path, string.upper(theType), model))
-                        end
-                        replaceList[k][theType.."_loaded"] = true
-                        checkReplaceCompleted("col")
-                    end
-                )
-			end
-            if not worked then
-            	outputDebugString("ABORTING: Decryption of '"..path.."' failed", 1)
-                return
-            end
-		end
+        local col = v.col
+        if col then
+            loadOneMod("col", col, model)
+        end
+        local txd = v.txd
+        if txd then
+            loadOneMod("txd", txd, model)
+        end
+        local dff = v.dff
+        if dff then
+            loadOneMod("dff", dff, model)
+        end
 	end
 end
 
-function applyLodDistances()
+local function applyLodDistances()
 
     for k,v in pairs(replaceList) do
         local model = v.model
@@ -96,11 +83,10 @@ end
 addEventHandler("onClientResourceStart", resourceRoot,
 function()
     if type(ncDecrypt) ~= "function" then
-        return outputDebugString("Decrypt function not loaded", 0, 255,0,0)
+        return outputDebugString("Nando Decrypt clientside function not loaded", 0, 255,0,0)
     end
 
     if not applyLodDistances() then return end
 
-    -- Correct load order: TXD -> DFF -> COL
-    replaceMods("txd")
+    replaceMods()
 end)
